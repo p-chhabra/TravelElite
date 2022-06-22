4e838a585510d27831ccf5579f6ad9cb7fc38945
prathmeshchhabra <p.chhabra2002@gmail.com>
2022-06-22 18:00:38 +0530

    Project Started

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Places from "./places/pages/Places";
+import Users from "./users/pages/Users";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Users/>}/>
+        <Route path="/places" element={<Places/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/places/pages/Places.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Places = () => {
+  return (
+    <div>
+        <h1>Places Page</h1>
+    </div>
+  )
+}
+
+export default Places
