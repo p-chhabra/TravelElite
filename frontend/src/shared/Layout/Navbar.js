14129e8e11b5f2048d48f9ca3f66d39818261620
prathmeshchhabra <p.chhabra2002@gmail.com>
2022-06-23 15:35:06 +0530

    Navbar designed

--- frontend/src/shared/Layout/Navbar.js
+++ frontend/src/shared/Layout/Navbar.js
@@ -12,19 +12,19 @@ const Navbar = () => {
     }
 
   return (
-    <nav className='w-full flex justify-between items-center px-4 font-bold bg-[#0b0c10]'>
-        <h1 className='text-[#66fcf1] text-3xl'>YourPlaces</h1>
-        <ul className='flex flex-row text-[#c5c6c7] p-5 items-center'>
-            <li className='px-6 hover:text-[#66fcf1] duration-500 text-sm'>
+    <nav className='w-full flex justify-between items-center px-4 font-bold bg-[white]'>
+        <h1 className='text-[#dc3545] text-3xl'>YourPlaces</h1>
+        <ul className='flex flex-row text-[#adb5bd] p-2 items-center space-x-6'>
+            <li className='px-4 hover:border-b-2 hover:border-red-600 hover:text-[black] duration-100 text-sm'>
                 <NavLink to="/">Home</NavLink>
             </li>
-            <li className='px-6 hover:text-[#66fcf1] duration-500 text-sm'>
+            <li className='px-4 hover:border-b-2 hover:border-red-600 hover:text-[black] duration-100 text-sm'>
                 <NavLink to="/users">All Users</NavLink>
             </li>
-            <li className='px-6 hover:text-[#66fcf1] duration-500 text-sm'>
+            <li className='px-4 hover:border-b-2 hover:border-red-600 hover:text-[black] duration-100 text-sm'>
                 <NavLink to="/places">SignIn</NavLink>
             </li>
-            <li className='px-6 border-2 rounded-md border-[#66fcf1] p-2 hover:text-[#66fcf1] duration-500 text-sm'>
+            <li className='px-4 border-2 border-red-600 rounded-md p-1 hover:text-[black] hover:bg-red-600 duration-500 text-sm my-2'>
                 <NavLink to="/places">SignUp</NavLink>
             </li>
         </ul>
