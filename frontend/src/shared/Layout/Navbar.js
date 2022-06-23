5b5b7d18946898003abb7b59f42107cd9c1b63ac
prathmeshchhabra <p.chhabra2002@gmail.com>
2022-06-23 23:38:02 +0530

    Navbar Changes Made

--- frontend/src/shared/Layout/Navbar.js
+++ frontend/src/shared/Layout/Navbar.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import { NavLink } from 'react-router-dom'
+import './Navbar.css'
 import {FaBars} from 'react-icons/fa'
 
 ///Navbar Component
@@ -7,24 +8,26 @@ const Navbar = () => {
 
     const [nav, setNav]  = useState(false);
 
+    //Function to open and close navbar when in mobile view
     const handleClick = () => {
         setNav(!nav);
     }
 
   return (
-    <nav className='w-full flex justify-between items-center px-4 font-bold bg-[white]'>
-        <h1 className='text-[#dc3545] text-3xl'>YourPlaces</h1>
-        <ul className='flex flex-row text-[#adb5bd] p-2 items-center space-x-6'>
-            <li className='px-4 hover:border-b-2 hover:border-red-600 hover:text-[black] duration-100 text-sm'>
+      ///Wide Device Navbar
+    <nav className='w-full flex justify-between items-center px-4 font-bold bg-[#21272c]'>
+        <h1 className='text-gray-300 text-3xl'>YourPlaces</h1>
+        <ul className='flex flex-row text-gray-300 p-2 items-center space-x-6'>
+            <li className='px-4 hover-underline-animation p-1 hover:text-green-400 duration-500 text-sm'>
                 <NavLink to="/">Home</NavLink>
             </li>
-            <li className='px-4 hover:border-b-2 hover:border-red-600 hover:text-[black] duration-100 text-sm'>
+            <li className='px-4 hover-underline-animation p-1 hover:text-green-400 duration-500 text-sm'>
                 <NavLink to="/users">All Users</NavLink>
             </li>
-            <li className='px-4 hover:border-b-2 hover:border-red-600 hover:text-[black] duration-100 text-sm'>
+            <li className='px-4 hover-underline-animation p-1 hover:text-green-400 duration-500 text-sm'>
                 <NavLink to="/places">SignIn</NavLink>
             </li>
-            <li className='px-4 border-2 border-red-600 rounded-md p-1 hover:text-[black] hover:bg-red-600 duration-500 text-sm my-2'>
+            <li className='px-4 border-2 border-green-400 rounded-md p-1 hover:text-[black] hover:bg-green-400 duration-500 text-sm my-2'>
                 <NavLink to="/places">SignUp</NavLink>
             </li>
         </ul>
