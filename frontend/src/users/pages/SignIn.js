d19c19a702874f09377bf9d42264a7df08955755
prathmeshchhabra <p.chhabra2002@gmail.com>
2022-06-29 22:53:22 +0530

    Form Validation added

--- frontend/src/users/pages/SignIn.js
+++ frontend/src/users/pages/SignIn.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import AnimatedPage from '../../shared/components/AnimatedPage'
 import "./SignIn.css"
+import { Link } from 'react-router-dom'
+
+///VALIDITY
+const isFiveChars = (value) => {
+  return value.trim().length === 5;
+}
+
+const emailValidation = (value) => {
+  return value.includes('@') && !value.includes(" ");
+} 
 
 const SignIn = () => {
+
+  const [formInputValidity, setFormInputValidity] = useState({
+    email: true,
+    password: true,
+  });
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const onSubmitHandler = (e) => {
+
+    e.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    const eneteredEmailIsValid = emailValidation(enteredEmail);
+    const eneteredPasswordIsValid = isFiveChars(enteredPassword);
+
+    setFormInputValidity({
+      email: eneteredEmailIsValid,
+      password: eneteredPasswordIsValid
+    })
+
+    const formIsValid = eneteredEmailIsValid && eneteredPasswordIsValid;
+
+    if(!formIsValid){
+      return;
+    }
+  }
+
   return (
     <AnimatedPage>
     <div className='flex justify-center pt-20'>
@@ -12,17 +53,20 @@ const SignIn = () => {
     <ul className="noBullet">
       <li>
         <label htmlFor="email"></label>
-        <input type="email" className="inputFields" id="email" name="email" placeholder="Email" autoComplete='off' required/>
+        <input ref={emailInputRef} type="email" className={formInputValidity.email ? `inputFields` : `inputFieldInvalid`} id="email" name="email" placeholder="Email" autoComplete='off' required/>
+        {!formInputValidity.email && <p className='text-red-500'>Please enter a valid email</p>}
       </li>
       <li>
         <label htmlFor="password"></label>
-        <input type="password" className="inputFields" id="password" name="password" placeholder="Password" required/>
+        <input ref={passwordInputRef} type="password" className={formInputValidity.password ? `inputFields` : `inputFieldInvalid`} id="password" name="password" placeholder="Password" required/>
+        {!formInputValidity.password && <p className='text-red-500'>Password should be longer than 5 characters</p>}
       </li>
       <li id="center-btn">
-        <input type="submit" id="join-btn" name="join" alt="Join" value="Submit"></input>
+        <input onClick={onSubmitHandler} type="submit" id="join-btn" name="join" alt="Join" value="Submit"></input>
       </li>
     </ul>
   </form>
+  <p className='alert'>//Dont have an account. Visit <Link className='text-green-300 underline' to='/signup'>SignUp</Link> Page to create one//</p>
   </div>
   </div>
   </AnimatedPage>
--- frontend/src/users/pages/SignUp.js
+++ frontend/src/users/pages/SignUp.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import AnimatedPage from '../../shared/components/AnimatedPage'
 import './SignUp.css'
 
+const emailValidation = value => value.includes('@') && value.length >= 5;
+const userNameValidation = value => !value.includes(" ") && value.length >= 4;
+const passwordValidation = value => !value.includes(" ") && value.length >= 5;
+
 const SignUp = () => {
+
+  const form = document.getElementById('SignUpForm');
+
+  const [formValidity, setFormValidity] = useState({
+    email: true,
+    username: true,
+    password: true
+  })
+
+  const emailInputRef = useRef();
+  const userNameInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const formValidityHandler = (e) => {
+
+    e.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+  const enteredUserName = userNameInputRef.current.value;
+  const enteredPassword = passwordInputRef.current.value;
+
+  const emailIsValid = emailValidation(enteredEmail);
+  const userNameIsValid = userNameValidation(enteredUserName);
+  const passwordIsValid = passwordValidation(enteredPassword);
+
+  setFormValidity({
+    email: emailIsValid,
+    username: userNameIsValid,
+    password: passwordIsValid
+  })
+
+  const formIsValid = emailIsValid && userNameIsValid && passwordIsValid;
+
+  if(!formIsValid) return;
+  form.reset();
+  }
+
+
   return (
     <AnimatedPage>
     <div className='flex items-center justify-center mt-20'>
@@ -12,27 +54,30 @@ const SignUp = () => {
     <i className="icon ion-ios-ionic-outline" aria-hidden="true"></i>
     <p>YourPlaces</p>
   </div>
-  <form action="#" method="POST" className="signupForm" name="signupform">
+  <form action="#" method="POST" id='SignUpForm' className="signupForm" name="signupform">
     <p className='text-2xl'>Sign Up</p>
     <ul className="noBullet">
       <li>
         <label htmlFor="username"></label>
-        <input type="text" className="inputFields" id="username" name="username" placeholder="Username" autoComplete='off'  required/>
+        <input ref={userNameInputRef} type="text" className={formValidity.username ? `inputFields` : `inputFieldInvalid`} id="username" name="username" placeholder="Username" autoComplete='off'  required/>
+        {!formValidity.username && <p className='text-red-500'>Username should be atleast 4 characters long</p>}
       </li>
       <li>
         <label htmlFor="email"></label>
-        <input type="email" className="inputFields" id="email" name="email" placeholder="Email" autoComplete='off' required/>
+        <input ref={emailInputRef} type="email" className={formValidity.email ? `inputFields` : `inputFieldInvalid`} id="email" name="email" placeholder="Email" autoComplete='off' required/>
+        {!formValidity.email && <p className='text-red-500'>Please enter a valid email</p>}
       </li>
       <li>
         <label htmlFor="password"></label>
-        <input type="password" className="inputFields" id="password" name="password" placeholder="Password" required/>
+        <input ref={passwordInputRef} type="password" className={formValidity.password ? `inputFields` : `inputFieldInvalid`} id="password" name="password" placeholder="Password" required/>
+        {!formValidity.password && <p className='text-red-500'>Password should be atleast 4 characters long</p>}
       </li>
       <h1 className='pt-10'>Profile Picture (Optional)</h1>
       <li>
         <input type="file" className='inputFields inputImageField' name="profile-pic" accept='image/png, image/jpeg, image/jpg'/>
       </li>
       <li id="center-btn">
-        <input type="submit" id="join-btn" name="join" alt="Join" value="Join"></input>
+        <input onClick={formValidityHandler} type="submit" id="join-btn" name="join" alt="Join" value="Join"></input>
       </li>
     </ul>
   </form>
