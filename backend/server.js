b9b2a2d5b3278f16666a5f2a5c88592d0116c4c7
prathmeshchhabra <p.chhabra2002@gmail.com>
2022-07-17 22:14:22 +0530

    connecting backend to frontend

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Access, Authorization');
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, DELETE');
+    next();
+})
+
 app.use('/api/places',placesRoutes);
 
 app.use('/api/users', userRoutes);
@@ -31,7 +38,7 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>');
 })
 
-const url = 'mongodb://van_astrea:abcdefgh@cluster0-shard-00-00.x7qxv.mongodb.net:27017,cluster0-shard-00-01.x7qxv.mongodb.net:27017,cluster0-shard-00-02.x7qxv.mongodb.net:27017/?ssl=true&replicaSet=atlas-4xumi5-shard-0&authSource=admin&retryWrites=true&w=majority'
+const url = 'mongodb://van_astrea:abcdefgh@cluster0-shard-00-00.x7qxv.mongodb.net:27017,cluster0-shard-00-01.x7qxv.mongodb.net:27017,cluster0-shard-00-02.x7qxv.mongodb.net:27017/mern?ssl=true&replicaSet=atlas-4xumi5-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 mongoose.connect(url).then(()=>{
     app.listen(5000);
